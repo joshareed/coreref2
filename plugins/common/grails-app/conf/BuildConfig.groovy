1157bf80a0029e7f47c9545278b0748341ad648c
Josh Reed <obitus9982@gmail.com>
2011-09-06 23:08:25 -0500

    Depend on the simple-mongo plugin

--- plugins/common/grails-app/conf/BuildConfig.groovy
+++ plugins/common/grails-app/conf/BuildConfig.groovy
@@ -1,3 +1,4 @@
+grails.plugin.location.'simple-mongo' = "../simple-mongo"
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
