081c82fb9a30dec7620dde16cecad97dfa100783
Josh Reed <obitus9982@gmail.com>
2011-09-15 20:55:49 -0500

    Consolidate the simple-mongo plugin into common

--- coreref/grails-app/conf/BuildConfig.groovy
+++ coreref/grails-app/conf/BuildConfig.groovy
@@ -1,4 +1,3 @@
-grails.plugin.location.'simple-mongo' = "../plugins/simple-mongo"
 grails.plugin.location.common = "../plugins/common"
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
@@ -15,19 +14,8 @@ grails.project.dependency.resolution = {
         grailsPlugins()
         grailsHome()
         grailsCentral()
-
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenLocal()
         //mavenCentral()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
     }
     dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-
-        // runtime 'mysql:mysql-connector-java:5.1.13'
     }
 }
