bbf2285d692b3a7060c6d5e22e622cde694a9426
Josh Reed <obitus9982@gmail.com>
2011-09-21 19:02:35 -0500

    Fixed some build issues with missing deps

--- services/grails-app/conf/BuildConfig.groovy
+++ services/grails-app/conf/BuildConfig.groovy
@@ -5,12 +5,17 @@ grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 grails.project.dependency.resolution = {
+    // inherit Grails' default dependencies
+    inherits("global") {
+        // uncomment to disable ehcache
+        excludes 'ehcache'
+    }
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     repositories {
         grailsPlugins()
         grailsHome()
         grailsCentral()
-        //mavenCentral()
+		mavenCentral()
     }
     dependencies {
     }
