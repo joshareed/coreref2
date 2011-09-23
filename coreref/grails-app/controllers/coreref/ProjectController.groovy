9c913c2606e7748e1700b1e8c5b678939532c19c
Josh Reed <obitus9982@gmail.com>
2011-09-23 00:43:42 -0500

    Project overview page

--- coreref/grails-app/controllers/coreref/ProjectController.groovy
+++ coreref/grails-app/controllers/coreref/ProjectController.groovy
@@ -1,6 +1,35 @@
 package coreref
 
-class ProjectController {
+import coreref.common.Project
 
-    def index = { }
+class ProjectController {
+	def lexiconService
+
+	private getProjects() { Project.mongoCollection }
+	private def withProject = { Map map, closure ->
+		def id = map.id
+		if (!id) {
+			flash.message = "No project id specified"
+			redirect uri: '/'
+			return
+		}
+
+		def project = projects.find(projectId: id)
+		if (!project) {
+			project = projects.get(id)
+			if (!project) {
+				flash.message = "No project with id '${id}'"
+				redirect uri: '/'
+				return
+			}
+		}
+
+		return closure.call(new Project(project))
+	}
+
+    def overview = {
+		withProject(params) { project ->
+			[project: project]
+		}
+	}
 }
---
+++ coreref/grails-app/views/project/overview.gsp
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>Project :: ${project.projectId}</title>
+		<meta name="layout" content="main" />
+	</head>
+	<body>
+		<h1>${project.name}</h1>
+		<div>
+			<span class="label warning">TODO</span> Project Page
+		</div>
+	</body>
+</html>
