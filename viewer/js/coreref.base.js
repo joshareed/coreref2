2275a3390190329340ff405a1aaf80c452d17a74
Josh Reed <josh@refactr.com>
2012-03-01 20:49:43 -0600

    Add ready callbacks on Stream and Project

--- viewer/js/coreref.base.js
+++ viewer/js/coreref.base.js
@@ -8,6 +8,25 @@ coreref.Stream = function(options) {
 	// private variables
 	var _this = this;
 	var _data = [];
+	var _ready = false;
+	var _readyQueue = [];
+
+	// handle json data
+	var handleData = function(json) {
+		for (var i = 0; i < json.data.length; i++) {
+			_this.add(json.data[i]);
+		}
+		if (json.paging && json.paging.next) {
+			jQuery.getJSON(root + json.paging.next, function(foo) {
+				handleData(foo);
+			});
+		} else {
+			_ready = true;
+			for (var i = 0; i < _readyQueue.length; i++) {
+				_readyQueue[i](_this);
+			}
+		}
+	}
 
 	// public fields
 	this.name = options.name;
@@ -24,23 +43,43 @@ coreref.Stream = function(options) {
 		}
 		_data.splice(insert, 0, item);
 	}
+	this.ready = function(func) {
+		if (_ready) {
+			func.call(this);
+		} else {
+			_readyQueue.push(func);
+		}
+	}
 
 	// initialization code
 	if (options.data) {
 		for (var i = 0; i < options.data.length; i++) {
 			this.add(options.data[i]);
 		}
+		_ready = true;
 	} else if (options.url) {
 		var root = options.root || '';
-		jQuery.getJSON(root + options.url, function(data) {
-			console.log(data);
+		jQuery.getJSON(root + options.url, function(json) {
+			handleData(json);
 		});
 	}
 }
 
 coreref.Project = function(options) {
 	// private vairables
+	var _this;
+	var _init = false;
+	var _readyCount = 0;
+	var _readyQueue = [];
 
+	var handleStreamReady = function(stream) {
+		_readyCount--;
+		if (_readyCount <= 0) {
+			for (var i = 0; i < _readyQueue.length; i++) {
+				_readyQueue[i](_this);
+			}
+		}
+	}
 
 	// public fields
 	this.id;
@@ -51,6 +90,9 @@ coreref.Project = function(options) {
 	this.root;
 
 	this.init = function(options) {
+		if (!options) { return; }
+		_init = true;
+		_this = this;
 		this.id = options.id || "";
 		this.name = options.name || "";
 		this.description = options.description || "";
@@ -64,10 +106,25 @@ coreref.Project = function(options) {
 				var s = options.streams[i];
 				if (s instanceof coreref.Stream) {
 					this.streams.push(s);
+					_readyCount++;
+					s.ready(handleStreamReady);
 				} else {
-					this.streams.push(new coreref.Stream(s));
+					if (this.root) {
+						s.root = this.root;
+					}
+					var stream = new coreref.Stream(s);
+					this.streams.push(stream);
+					_readyCount++;
+					stream.ready(handleStreamReady);
 				}
 			}
+		}
+	}
+	this.ready = function(func) {
+		if (_init && _readyCount <= 0) {
+			func(this);
+		} else {
+			_readyQueue.push(func);
 		}
 	}
 
